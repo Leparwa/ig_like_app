832fd0879efd6d5a420c785a553cea3226ede094
leparwa <kevin.leparwa@student.moringaschool.com>
2021-09-13 12:20:25 +0300

    deployment

--- instaclone/views.py
+++ instaclone/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
-@login_required(login_url='/accounts/login/')
+# @login_required(login_url='/accounts/login/')
 def home(request):
     return render(request, 'ig/home.html')
 
