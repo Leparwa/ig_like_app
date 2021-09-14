011177ddf2627f639d7e1ce67ad82ec43f2093fc
leparwa <kevin.leparwa@student.moringaschool.com>
2021-09-14 11:40:17 +0300

    production

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path("profile/", views.profile, name="profile"),
     path("register/", views.register, name="register"),
+    path("edit-profile/", views.update_profile, name="edit-profile"),
     path('accounts/', include('django.contrib.auth.urls')),
     ]
--- instaclone/views.py
+++ instaclone/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 from django.contrib.auth import login
 from django.shortcuts import redirect, render
 from django.urls import reverse
-from .forms import CustomUserCreationForm
+from django.contrib import messages
+from django.db import transaction
+from .forms import CustomUserCreationForm, ProfileForm, UserForm, ProfileForm
 
 @login_required(login_url='/accounts/login/')
 def home(request):
@@ -25,4 +27,23 @@ def register(request):
             user = form.save()
             login(request, user)
         return redirect(reverse("home"))
+
+@login_required
+@transaction.atomic
+def update_profile(request):
+    print('updating user')
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        if user_form.is_valid():
+            user_form.save()
+            messages.success(request, ('Your profile was successfully updated!'))
+            return redirect('profile')
+        else:
+            messages.error(request, ('Please correct the error below.'))
+    else:
+        user_form = UserForm(instance=request.user)
+    return render(request, 'ig/edit-user.html', {
+        'user_form': user_form,
+    })
 
