be509634dde4aef32d1813d9bd687e9c76567fa1
leparwa <kevin.leparwa@student.moringaschool.com>
2021-09-15 22:27:18 +0300

    added user post

--- instaclone/views.py
+++ instaclone/views.py
@@ -4,16 +4,16 @@ from django.contrib.auth import login
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.contrib import messages
-from .models import Image
+from .models import Image, Profile
 from django.db import transaction
+from django.forms import modelformset_factory
 from .forms import CustomUserCreationForm, ImageForm, ProfileForm, UserForm, ProfileForm
 
 @login_required(login_url='/accounts/login/')
 def home(request):
     images= Image.objects.all()
-
-    print(images)
-    return render(request, 'ig/home.html', context ={'images': images })
+    profiles = Profile.objects.all().exclude(user=request.user)
+    return render(request, 'ig/home.html', context ={'images': images, 'profiles':profiles })
 
 @login_required(login_url='/accounts/login/')
 def profile(request):
@@ -21,10 +21,7 @@ def profile(request):
 
 def register(request):
     if request.method == "GET":
-        return render(
-            request, "users/register.html",
-            {"form": CustomUserCreationForm}
-        )
+        return render(request, "users/register.html",{"form": CustomUserCreationForm})
     elif request.method == "POST":
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
@@ -37,26 +34,24 @@ def register(request):
 def update_profile(request):
     if request.method == 'POST':
         user_form = UserForm(request.POST, instance=request.user)
-        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
         if user_form.is_valid():
             user_form.save()
             messages.success(request, ('Your profile was successfully updated!'))
-            return redirect('profile')
-        else:
-            messages.error(request, ('Please correct the error below.'))
-    else:
-        user_form = UserForm(instance=request.user)
-    return render(request, 'ig/edit-user.html', {
-        'user_form': user_form,
-    })
+        elif profile_form.is_valid():
+            profile_form.save() 
+        return redirect('profile')
+
+    profile_form = ProfileForm(instance=request.user.profile)
+    user_form = UserForm(instance=request.user)
+    return render(request, 'ig/edit-user.html', {'user_form': user_form, "profile_form": profile_form})
 
 @login_required
 def new_post(request):
-    """Process images uploaded by users"""
     if request.method == 'POST':
-        profile_form = ImageForm(request.POST, request.FILES)
-        if profile_form.is_valid():
-            form = profile_form.save(commit=False)
+        image_form = ImageForm(request.POST, request.FILES)
+        if image_form.is_valid():
+            form = image_form.save(commit=False)
             form.profile = request.user.profile
             form.save()
             return redirect('home')
